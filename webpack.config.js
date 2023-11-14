8259fa00720489cba5e4f1030f711e105f4569fb
Thomas Tilak <thomas.tilak@gmail.com>
2023-11-14 16:24:13 +1100

    handle sourcemap file for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'parser.js',
-    library: "parser",
-    libraryTarget: 'umd',
-    globalObject: 'this'
-  },
+module.exports = (env, argv) => {
+  let config = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'parser.js',
+      library: "parser",
+      libraryTarget: 'umd',
+      globalObject: 'this'
+    },
+  };
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+  return config;
 };
